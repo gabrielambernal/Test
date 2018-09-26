68b7cec3bc0d2107181da4a2c290e5ae60e0d72f
unknown <gabrielamdzbernal@gmail.com>
2018-09-26 16:39:04 -0500

    Quiero irme a dormir

--- php/dbcon.php
+++ php/dbcon.php
@@ -3,7 +3,7 @@ $servername = "localhost";
 $username = "root";
 $password = "";
 $dbname = "laptopassignation";
-
+//HOLA
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 
